95e557779e0e26c23aaa6bac7405a08a863394fc
NicolasPegolo30 <nicolaspegolo@hotmail.com>
2023-07-03 21:35:03 -0300

    Removed github respos

--- Assets/CustomScriptCreator/Framework/FrameworkHelp.cs
+++ Assets/CustomScriptCreator/Framework/FrameworkHelp.cs
@@ -6,9 +6,6 @@ namespace CustomScriptsCreator
     #if UNITY_EDITOR
     internal class FrameworkHelp
     { 
-        [MenuItem("Help/Custom Scripts/Open github repos...")]
-        static void OpenGitRepos() => Application.OpenURL("https://github.com/Manteiga30/ScriptsCreatorPackge"); 
-
         [MenuItem("Help/Custom Scripts/Debug contact email...")]
         static void DebugEmail() => Debug.Log("nicolaspegolo@hotmail.com"); 
 
